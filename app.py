2763eddf2b6d717f08a4986e0884cb21524f35a8
murilocesf <162130133+murilocesf@users.noreply.github.com>
2024-04-01 12:10:26 -0300

    subir arquivos

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Programa Expresso/m")
+print("Cadastrar Restaurante")
+print("Listar restaurante")
+print("ativar restaurante")
+print("abrir/n")
+
+
+opcao_digitada=input
+print("você escolheu:", opcao_digitada)
+
+
+opcao_digitada=input
+print("você escolheu:", opcao_digitada)
+
+if(opcao_digitada==1):
+    print("você escolheu cadastrar um restaurante\n")
+elif (opcao_digitada==2):
+    print("você escolheu listar os restaurantes do app\n")
+elif(opcao_digitada==3):
+    print("você escolheu ativar um restaurante\n")
+elif(opcao_digitada==4):
+    print("você escolhey sair do programa\n")
